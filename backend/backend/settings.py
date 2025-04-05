546427ddae80579b917fb432cf1ba4ce99d4d6f5
Ken <122612391+pxiaccount@users.noreply.github.com>
2025-04-05 10:35:54 +0700

    Add fetch function from api and random element

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Add these lines at the bottom of the file
+CORS_ALLOW_ALL_ORIGINS = True  # Only for development!
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Your React app URL
+]
--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Home = () => {
+    const API_URL = 'http://localhost:8000/api/'
+    const [atomicNumber, setAtomicNumber] = useState({ number: [] })
+    const [randomElement, setRandomElement] = useState(null)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const result = await axios.get(API_URL)
+                setAtomicNumber({
+                    number: result.data
+                })
+            } catch (error) {
+                console.error('Error fetching data:', error)
+            }
+        }
+
+        fetchData()
+    }, [])
+
+    const getRandomElement = () => {
+        const randomI = Math.floor(Math.random() * atomicNumber.number.length)
+        setRandomElement(atomicNumber.number[randomI])
+        console.log(atomicNumber.number[randomI])
+    }
+
     return (
         <>
             <h1>Random Perodic Table</h1>
-            <div id="atomicnumber"></div>
-            <input type="text" />
+            <div id="atomicnumber">
+                {randomElement && (
+                    <div>{randomElement.atomic_number}</div>
+                )}
+            </div>
+            <input type="text" placeholder='Enter element name...' />
+            <button onClick={getRandomElement}>Random</button>
+            <button>Submit</button>
         </>
     )
 }
